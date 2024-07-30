83047e76c749d925c88f193030cfd5f2c58221cd
Dario Gabriel <dariogabriel113@gmail.com>
2024-07-30 14:11:06 -0300

    Add metodo cvonverteDolarParaReal

--- desafio3/app.js
+++ desafio3/app.js
@@ -4,9 +4,15 @@ function calculaIMC(alturaEmMetros, pesoEmQuilogramas) {
 
 function calculaFatorial(valor) {
     let resultado = 1;
-    for(i = valor; i > 0; i--) {
+    for (i = valor; i > 0; i--) {
         resultado = resultado * i
     }
 
     return resultado;
+}
+
+function converteDolarParaReal(valorEmDolar) {
+    let cotacao = 4.18;
+    let valorConvertido = valorEmDolar * cotacao;
+    return `R$ ${valorConvertido.toString().replace('.', ',')}`;
 }
