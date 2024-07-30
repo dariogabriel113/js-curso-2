5cec1346ca843202292400b771ab16ce15dd9f86
Dario Gabriel <dariogabriel113@gmail.com>
2024-07-30 14:04:22 -0300

    Add metodo calculaFatorial

--- desafio3/app.js
+++ desafio3/app.js
@@ -1,3 +1,12 @@
 function calculaIMC(alturaEmMetros, pesoEmQuilogramas) {
     return pesoEmQuilogramas / (alturaEmMetros * alturaEmMetros);
 }
+
+function calculaFatorial(valor) {
+    let resultado = 1;
+    for(i = valor; i > 0; i--) {
+        resultado = resultado * i
+    }
+
+    return resultado;
+}
