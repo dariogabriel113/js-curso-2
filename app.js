f5bf2952cd8f1c32a8cf4b4a67c94e1c10b7e26b
Dario Gabriel <dariogabriel113@gmail.com>
2024-07-30 13:42:06 -0300

    Incluir metodo reiniciarJogo e reorganizar metodos

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto;
+let tentativas;
 
 function exibirNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirNaTela('h1', 'Jogo do número secreto');
-exibirNaTela('p',  'Escolha um número entre 1 e 10');
+function gerarNumeroAleatorio(params) {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    let chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function exibirMensagensIniciais() {
+    exibirNaTela('h1', 'Jogo do número secreto');
+    exibirNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+function reiniciarJogo() {
+    exibirMensagensIniciais();
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+reiniciarJogo();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
 
-    if(chute == numeroSecreto) {
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativas > 1 ? 'tetativas' : 'tentativa'
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+
         exibirNaTela('h1', 'Acertou');
-        exibirNaTela('p', 'Você descobriu o número secreto!');
+        exibirNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
-        if(chute > numeroSecreto) {
+        if (chute > numeroSecreto) {
             exibirNaTela('p', 'O número secreto é menor');
         } else {
             exibirNaTela('p', 'O número secreto é maior');
         }
-    }
-}
 
-function gerarNumeroAleatorio(params) {
-    return parseInt(Math.random() * 10 + 1);
+        tentativas++;
+        limparCampo();
+    }
 }
